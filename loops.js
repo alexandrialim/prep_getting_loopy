68f033ee696ced7aa0c581513b2e90da3672eddd
alexandrialim <limria2011@gmail.com>
2016-06-24 14:48:56 -1000

    challenge 1

--- loops.js
+++ loops.js
@@ -1,1 +1,11 @@
 // Do your work here
+var arr = ["Washington", "Adams", "Jefferson", "Madison", "Monroe"];
+function presidentsArr(presidents){
+for (var i = 0; i < presidents.length; i++){
+  
+  console.log ("The value of i is:" + i);
+  
+  console.log (("The value at index is:" + arr[i]));
+}
+}
+presidentsArr(arr);
